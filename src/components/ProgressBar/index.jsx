e84453cf568051c08d8521205be3f174272339ec
david <davidyuzhakov17@gmail.com>
2024-10-01 08:49:26 +0300

    fix bug progressbar

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -2,13 +2,14 @@ import styles from './ProgressBar.module.scss'
 
 const TOTAL_ITEMS = 82
 
-export function ProgressBar ({ segements }) {
+export function ProgressBar ({ segments }) {
+
   return (
     <div className={styles.progressBar}>
       {Array.from({ length: TOTAL_ITEMS }, (_, i) => (
         <span
           key={i}
-          className={i <= segements ? styles.fill : ''}
+          className={i <= segments ? styles.fill : ''}
         />
       ))}
     </div>
